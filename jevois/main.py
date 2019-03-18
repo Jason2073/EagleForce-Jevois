73b926398fa9798d44f4f11eb9e95ae642203e93
jason2073 <stanleytechrepair@gmail.com>
2019-03-17 19:18:14 -0700

    Adding run configs and basic hsv calibration.

--- jevois/main.py
+++ jevois/main.py
@@ -1,10 +1,25 @@
 import cv2
-cap = cv2.VideoCapture("/dev/video1")
+import numpy as np
+cap = cv2.VideoCapture("/dev/video0")
 while True:
     ret, frame = cap.read()
-    cv2.imshow('img', frame)
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_blue = np.array([80, 50, 50])
+    upper_blue = np.array([100, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask)
+    cv2.imshow('res', res)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+
 
 cap.release()
 cv2.destroyAllWindows()
